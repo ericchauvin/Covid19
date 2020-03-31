d77653abd4dc23423730cde96395a33a6a5efdca
Eric Chauvin <chauvineric4@gmail.com>
2020-03-30 19:40:07 -0600

    Update CovidRecordArray.java

--- CovidRecordArray.java
+++ CovidRecordArray.java
@@ -1,8 +1,13 @@
 // Copyright Eric Chauvin 2020.
 
 
+
+// Do a previous data file and the change amounts.
+
+
 public class CovidRecordArray
   {
+  private MainApp mApp;
   private CovidRecord[] covidRecArray;
   private int[] sortIndexArray;
   private int arrayLast = 0;
@@ -100,6 +105,81 @@ Sort by change, by totals... whatever.
     return switched;
     }
 */
+
+
+
+  public void readFromFiles()
+    {
+    try
+    {
+    String mainDir = "C:\\Eric\\Covid19\\";
+
+    String fileName = mainDir + "Covid19Data.txt";
+
+    String dataS = FileUtility.readFileToString(
+                                        mApp,
+                                        fileName,
+                                        false );
+
+    if( dataS.length() == 0 )
+      {
+      mApp.showStatus( " " );
+      mApp.showStatus( "There is nothing in the file." );
+      mApp.showStatus( fileName );
+      return;
+      }
+
+    // mApp.showStatus( dataS );
+
+    StringArray sArray = new StringArray();
+    int max = sArray.makeFieldsFromString( dataS,
+                                           '\n' );
+
+    int totalDeaths = 0;
+    for( int count = 0; count < max; count++ )
+      {
+      String line = sArray.getStringAt( count );
+      if( line.contains( "FIPS,Admin2,Province_State," ))
+        continue;
+
+      CovidRecord rec = new CovidRecord( mApp );
+      if( !rec.setFieldsFromFileLine( line ))
+        continue;
+      
+      if( rec.FIPS.length() == 0 )
+        continue;
+
+      totalDeaths += rec.Deaths;
+      if( rec.Province_State.equals( "Colorado" ))
+        mApp.showStatus( rec.makeShowString());
+
+      addRecord( rec );
+      }
+
+    mApp.showStatus( " " );
+    mApp.showStatus( "Total deaths: " + totalDeaths );
+
+    mApp.showStatus( " " );
+    mApp.showStatus( "Finished processing the file." );
+    mApp.showStatus( " " );
+    }
+    catch( Exception e )
+      {
+      mApp.showStatus( "Exception in readFromFiles()." );
+      mApp.showStatus( e.getMessage() );
+      }
+    }
+
+
+
+  public void addRecord( CovidRecord rec )
+    {
+    if( arrayLast >= sortIndexArray.length )
+      resizeArrays( 1024 );
+
+    covidRecArray[arrayLast] = rec;
+    arrayLast++;
+    }
 
 
 
